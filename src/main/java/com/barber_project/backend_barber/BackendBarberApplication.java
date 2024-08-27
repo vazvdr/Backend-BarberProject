8a589d46c677d92f5b15a0073e9611156160c2c2
vazvdr <azevedo.vanderson@hotmail.com>
2024-08-27 11:28:05 -0300

    Projeto criado

---
+++ src/main/java/com/barber_project/backend_barber/BackendBarberApplication.java
@@ -0,0 +1,13 @@
+package com.barber_project.backend_barber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBarberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barber_project/backend_barber/BackendBarberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barber_project.backend_barber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
