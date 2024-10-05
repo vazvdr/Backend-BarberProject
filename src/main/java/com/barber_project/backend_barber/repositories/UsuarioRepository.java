09da7d39153a51d455057ae467be83ea78ae7bb0
vazvdr <azevedo.vanderson@hotmail.com>
2024-10-05 12:46:53 -0300

    Configurado com profile dev e salvando os dados no Postgresql

--- src/main/java/com/barber_project/backend_barber/repositories/UsuarioRepository.java
+++ src/main/java/com/barber_project/backend_barber/repositories/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package com.barber_project.backend_barber.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.barber_project.backend_barber.entities.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	Optional<Usuario> findByEmail(String email);
 
 }
